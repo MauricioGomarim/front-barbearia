5348297680c41c0099fc7d9ef6448081eef8b90b
Mauricio Gomarim <mauricio.gomarim3@gmail.com>
2024-03-12 20:47:56 -0300

    bug socket io

--- src/pages/Painel/QrcodePage/index.jsx
+++ src/pages/Painel/QrcodePage/index.jsx
@@ -7,20 +7,31 @@ import { useEffect, useState } from "react";
 import QRCode from "qrcode.react";
 import robo_green from "../../../assets/green.png";
 import robo_red from "../../../assets/red.jfif";
+import io from 'socket.io-client'
+
 
 export function QrcodePage() {
   const { menuActive, setMenuActive } = useAuth();
   const [qrCode, setQrCode] = useState("");
-
+  console.log(qrCode)
+  // console.log(qrCode)
+  
   useEffect(() => {
-    const fetchQRCode = async () => {
-      const response = await api.get("/qrcode");
-      console.log(response);
-      setQrCode(response.data.qrCode);
-    };
-
-    fetchQRCode();
+    function conecao(){
+      io.connect('http://localhost:3333')
+    }
+    conecao()
   }, []);
+
+  // useEffect(() => {
+  //   const fetchQRCode = async () => {
+  //     const response = await api.get("/qrcode");
+  //     console.log(response);
+  //     setQrCode(response.data.qrCode);
+  //   };
+
+  //   fetchQRCode();
+  // }, []);
   return (
     <Container
       style={{ gridTemplateColumns: menuActive ? "250px auto" : "80px auto" }}
@@ -29,6 +40,7 @@ export function QrcodePage() {
       <Menu />
       <div className="content flex flex-col justify-center items-center">
         <h1 className="text-[25px] mb-10 text-black font-bold">
+          <h1>{qrCode} teste</h1>
           {qrCode == 'autenticado' ? (
   <h1>Autenticado!</h1>
           ) : (<h1>Escaneie o QRCode para ativar o robo de notificações!</h1>)}
@@ -45,6 +57,8 @@ export function QrcodePage() {
           <div className="w-full lg:w-3/6 flex flex-col items-center">
             {qrCode == "autenticado" ? (
               <QRCode value={''} size={400} className="opacity-50" />
+            ) : qrCode == "gerando" ? (
+              <><h1>Gerando QRCode</h1></>
             ) : (
               <QRCode value={qrCode} size={400} />
             )}
